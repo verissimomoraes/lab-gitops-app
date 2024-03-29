1dd3e01f228504cf6105f60c9d566c48eac1789c
verissimomoraes <verissimomoraes@gmail.com>
2024-03-28 22:51:57 -0300

    subindo wp-config env

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '$WORDPRESS_DB_NAME');
+define( 'DB_NAME', getenv('WORDPRESS_DB_NAME'));
 
 /** MySQL database username */
-define( 'DB_USER', '$WORDPRESS_DB_USER');
+define( 'DB_USER', getenv('WORDPRESS_DB_USER'));
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '$WORDPRESS_DB_PASSWORD');
+define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
 
 /** MySQL hostname */
-define( 'DB_HOST', '$WORDPRESS_DB_HOST');
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
